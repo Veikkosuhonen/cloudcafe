57e7767721a65556094eb25c3b201001b49c6dbf
Veikkosuhonen <vesuvesuh@gmail.com>
2024-12-14 18:30:16 +0200

    Database & configs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
-use cloudcafe::run;
+use cloudcafe::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to 8000");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to address");
 
     run(listener)?.await
 }

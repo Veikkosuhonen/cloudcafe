56329c5835bc2f8ecbeb8db01b882f8217935b3d
Veikkosuhonen <vesuvesuh@gmail.com>
2024-12-14 23:46:45 +0200

    Acually insert something into db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,15 @@
-use sqlx::{PgConnection, Connection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get(format!("{}/health_check", address))
+        .get(format!("{}/health_check", app.address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -22,18 +22,14 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscibe_returns_a_200_for_valid_form_data() {
     // Arrange
-    let app_address = spawn_app();
-    let configuration = cloudcafe::configuration::get_configuration().expect("Failed to read configuration.");
-    let mut connection = PgConnection::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // Act
     let response = client
-        .post(&format!("{}/subscribe", app_address))
+        .post(&format!("{}/subscribe", app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -44,7 +40,7 @@ async fn subscibe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection)
+        .fetch_one(&app.connection_pool)
         .await
         .expect("Failed to fetch saved subscription");
 
@@ -55,7 +51,7 @@ async fn subscibe_returns_a_200_for_valid_form_data() {
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
-    let app_address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -66,7 +62,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     for (invalid_body, error_message) in test_cases {
         // Act
         let response = client
-            .post(&format!("{}/subscribe", app_address))
+            .post(&format!("{}/subscribe", app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -83,12 +79,26 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     }
 }
 
-fn spawn_app() -> String {
+struct TestApp {
+    address: String,
+    connection_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
 
-    let server = cloudcafe::startup::run(listener).expect("Failed to bind address");
+    let configuration = cloudcafe::configuration::get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let server = cloudcafe::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        connection_pool,
+    }
 }
